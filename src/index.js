74fc22ecb4293a85b06082389a6a9b4376f4fa73
Ab <77987005+securing-ab@users.noreply.github.com>
2022-08-27 13:16:46 +0300

    prettieir and eslint

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 const fetch = require('node-fetch')
 const config = require('./config.json')
 
- async function getLyrics(song, service){
+async function getLyrics(song, service) {
   let needed = 'genius' || 'finder'
-    if(service === undefined ||service !== needed ){
-        service = 'genius'
-    }
-    if(song === undefined){
-        throw new Error("Please provide a song.")
-    }
-    let get = await fetch(`${config.baseURL}/v3/lyrics/${service}/${song}`)
-    let resp = await get.text()
-    let res = JSON.parse(resp).lyrics
-    return res
+  if (service === undefined || service !== needed) {
+    service = 'genius'
   }
+  if (song === undefined) {
+    throw new Error('Please provide a song.')
+  }
+  let get = await fetch(`${config.baseURL}/v3/lyrics/${service}/${song}`)
+  let resp = await get.text()
+  let res = JSON.parse(resp).lyrics
+  return res
+}
 
-  async function getGuildData(id){
-    if(id !== Number ){
-        throw new Error("The ID you have provided is NOT a number")
-    }else if(id === undefined){
-        throw new Error("Please provide a guild ID.")
-    }
-    let get = await fetch(`${config.baseURL}/v3/data/guilds/${id}`)
-    let resp = await get.json()
-    let res = JSON.parse(JSON.stringify(resp))
-    let res1 = res.role
-    let res2 = res.channel
-    return {
-       res1,
-       res2 
-    }
+async function getGuildData(id) {
+  if (id !== Number) {
+    throw new Error('The ID you have provided is NOT a number')
+  } else if (id === undefined) {
+    throw new Error('Please provide a guild ID.')
   }
-module.exports = {getLyrics, getGuildData}
+  let get = await fetch(`${config.baseURL}/v3/data/guilds/${id}`)
+  let resp = await get.json()
+  let res = JSON.parse(JSON.stringify(resp))
+  let res1 = res.role
+  let res2 = res.channel
+  return {
+    res1,
+    res2
+  }
+}
+module.exports = { getLyrics, getGuildData }
--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,8 @@
-let {getLyrics, getGuildData} = require('../dist/index')
-async function find(){
-let lyrics = await getLyrics('on my way')
-console.log(lyrics)
-let get = await getGuildData('989559473104359504')
-console.log(get)
+let { getLyrics, getGuildData } = require('../dist/index')
+async function find() {
+  let lyrics = await getLyrics('on my way')
+  console.log(lyrics)
+  let get = await getGuildData('989559473104359504')
+  console.log(get)
 }
 find()
