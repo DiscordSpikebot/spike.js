850cb03d76681f7f4898c85998d640bd1741265b
Ab <77987005+securing-ab@users.noreply.github.com>
2022-11-04 18:28:07 +0200

    other: fix examples to return buffer

--- tests/index.js
+++ tests/index.js
@@ -1,18 +1,21 @@
 const { Client } = require('../dist/index.js')
 const { songInfoCard } = require('../dist/index.js')
-const img = new songInfoCard({
-  thumbnail: 'https://cdn.discordapp.com/attachments/913885039480152144/1008433692529672232/Logo_2.png',
-  title: 'e',
-  author: 'e',
-  duration: 'e',
-  source: 'yt'
+const imgcreate = new songInfoCard({
+  debug: true
 })
-console.log(img)
 let client = new Client({
   version: 'v3'
 })
 
 async function find() {
+  const img = await imgcreate.generate({
+    thumbnail: 'https://cdn.discordapp.com/attachments/913885039480152144/1008433692529672232/Logo_2.png',
+    title: 'e',
+    author: 'e',
+    duration: 'e',
+    source: 'yt'
+  })
+  console.log(img)
   console.log(await client.getGuildData('989559473104359504'))
   console.log(await client.getLyrics('1 day 2 nights'))
 }
